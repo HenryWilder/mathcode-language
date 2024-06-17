c596d1cdef760ffe342fbfcd3598f4a16d30efad
Henry Wilder <henrythepony@gmail.com>
2024-06-17 15:26:39 -0400

    Modified compiler slightly

--- src/compiler.rs
+++ src/compiler.rs
@@ -163,23 +163,25 @@ pub enum Comment {
 }
 
 pub enum Instruction {
-    Symbol (Symbol ),
-    Macro  (Macro  ),
-    Var    (String ),
-    Num    (i32    ),
-    Scope  (Scope  ),
+    Sym  (Symbol ),
+    Cmd  (Macro  ),
+    Var  (String ),
+    Txt  (String ),
+    Num  (i32    ),
+    Scope(Scope  ),
     Newline,
 }
 
 impl Display for Instruction {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
         write!(f, "{}", match self {
-            Instruction::Symbol(x)  => format!("Symbol({x:?})"),
-            Instruction::Macro(x)   => format!( "Macro({x:?})"),
-            Instruction::Var(x)     => format!(   "Var({x:?})"),
-            Instruction::Num(x)     => format!(   "Num({x:?})"),
-            Instruction::Scope(x)   => format!( "Scope({x:?})"),
-            Instruction::Newline    => "Newline".to_owned(),
+            Instruction::Sym(x)   => format!(  "Sym({x:?})"),
+            Instruction::Cmd(x)   => format!(  "Cmd({x:?})"),
+            Instruction::Var(x)   => format!(  "Var({x:?})"),
+            Instruction::Txt(x)   => format!(  "Txt({x:?})"),
+            Instruction::Num(x)   => format!(  "Num({x:?})"),
+            Instruction::Scope(x) => format!("Scope({x:?})"),
+            Instruction::Newline  => "Newline".to_owned(),
         })
     }
 }
@@ -194,14 +196,14 @@ impl FromStr for Instruction {
 
         if re_macro.is_match(s) {
             match Macro::from_str(s){
-                Ok(m) => Ok(Instruction::Macro(m)),
+                Ok(m) => Ok(Instruction::Cmd(m)),
                 Err(err) => Err(err),
             }
         } else if re_var.is_match(s) {
             Ok(Instruction::Var(s.into()))
         } else if re_op.is_match(s) {
             match Symbol::from_str(s) {
-                Ok(sym) => Ok(Instruction::Symbol(sym)),
+                Ok(sym) => Ok(Instruction::Sym(sym)),
                 Err(()) => Err(format!("\"{s}\" is not a recognized symbol")),
             }
         } else if re_num.is_match(s) {
@@ -213,7 +215,7 @@ impl FromStr for Instruction {
 }
 
 pub struct Statement {
-    instructions: Vec<Instruction>,
+    pub instructions: Vec<Instruction>,
 }
 
 impl Statement {
@@ -223,7 +225,7 @@ impl Statement {
 }
 
 pub struct Program {
-    statements: Vec<Statement>,
+    pub statements: Vec<Statement>,
 }
 
 impl Program {
@@ -260,13 +262,14 @@ r"\documentclass{article}
             // result.push_str(r"&");
             for instruction in &statement.instructions {
                 let instruction_string: String = match instruction {
-                    Instruction::Symbol(x) => x.to_string(),
-                    Instruction::Macro (x) => x.to_string(),
-                    Instruction::Var   (x) => format!("{x}"),
-                    Instruction::Num   (x) => format!("{x}"),
-                    Instruction::Scope (x) => match x {
-                        &Scope::Pop  => "}".into(),
-                        &Scope::Push => "{".into(),
+                    Instruction::Sym(x) => x.to_string(),
+                    Instruction::Cmd(x) => x.to_string(),
+                    Instruction::Var(x) => format!("{x}"),
+                    Instruction::Num(x) => format!("{x}"),
+                    Instruction::Txt(x) => format!(r"\text{{{x}}}"),
+                    Instruction::Scope(x) => match x {
+                        Scope::Pop  => "}".into(),
+                        Scope::Push => "{".into(),
                     },
                     Instruction::Newline => r"\\".into(),
                 };
@@ -281,48 +284,159 @@ r"\end{align*}
     }
 }
 
+/// Comment
+const RE_COM: &str = r"(%.*?)\n";
+
+/// Newline
+const RE_NLI: &str = r"(\\\\)";
+
+/// End of statement
+const RE_END: &str = r"(;)";
+
+/// Number literal
+const RE_NUM: &str = r"(\d+\.?\d*|\.\d+)";
+
+/// Text literal
+const RE_TXT: &str = r"\\text\{(.*?)\}";
+
+/// Braces
+const RE_BRA: &str = r"(\\\{|\\\})";
+
+/// Scope
+const RE_SCO: &str = r"(\{|\})";
+
+/// Variable
+const RE_VAR: &str = r"([A-Za-z]'*)";
+
+/// Command
+const RE_CMD: &str = r"(\\[A-Za-z]+)";
+
+/// Symbol
+const RE_SYM: &str = r"([`~!@#$%\^&*()[\\]<>,./?:'|\-_=+])";
+
+const RE_COM_ID: usize = 1;
+const RE_NLI_ID: usize = RE_COM_ID + 1;
+const RE_END_ID: usize = RE_NLI_ID + 1;
+const RE_NUM_ID: usize = RE_END_ID + 1;
+const RE_TXT_ID: usize = RE_NUM_ID + 1;
+const RE_BRA_ID: usize = RE_TXT_ID + 1;
+const RE_SCO_ID: usize = RE_BRA_ID + 1;
+const RE_VAR_ID: usize = RE_SCO_ID + 1;
+const RE_CMD_ID: usize = RE_VAR_ID + 1;
+const RE_SYM_ID: usize = RE_CMD_ID + 1;
+
+const RE_OPTS: [&str;10] = [
+    RE_COM,
+    RE_NLI,
+    RE_END,
+    RE_NUM,
+    RE_TXT,
+    RE_BRA,
+    RE_SCO,
+    RE_VAR,
+    RE_CMD,
+    RE_SYM,
+];
+
 pub fn compile(code: String) -> Result<Program,String> {
-    let re = Regex::new(r"(%.*?\n|\d+\.?\d*|\.\d+|[A-Za-z]'*|\\\{|\\\}|\\\\|\\[A-Za-z]+|[`~!@#$%^&*()[\\]{}<>,./?:;'|\-_=+])").unwrap();
+    let re_word = Regex::new(RE_OPTS.join("|").as_str()).unwrap();
 
     let mut program = Program::new();
     program.push_statement();
-    for captures in re.captures_iter(&code) {
-        let word = captures.get(0).unwrap().as_str().trim();
+    for captures in re_word.captures_iter(&code) {
+        let word = captures.get(0).unwrap().as_str();
         print!("\"{word}\": ");
-        if word.starts_with('%') {
+
+        // debug
+        let mut pattern_number = 0;
+        for i in 1..=9 {
+            if captures.get(i).is_some() {
+                pattern_number = i;
+                break;
+            }
+        }
+        let pattern_id = match pattern_number {
+            0 => "unknown",
+            RE_COM_ID => "comment",
+            RE_NLI_ID => "newline",
+            RE_END_ID => "end statement",
+            RE_NUM_ID => "number literal",
+            RE_TXT_ID => "text literal",
+            RE_BRA_ID => "brace literal",
+            RE_SCO_ID => "scope",
+            RE_VAR_ID => "variable",
+            RE_CMD_ID => "command",
+            RE_SYM_ID => "symbol",
+            _ => unreachable!(),
+        };
+        print!("pattern {pattern_number} ({pattern_id}): ");
+
+        if captures.get(RE_COM_ID).is_some() {
             println!("[COMMENT]");
             continue;
-        }
-        match word {
-            ";" => {
-                if program.is_current_statement_empty().unwrap_or(true) {
-                    println!("[END OF (empty) STATEMENT (ignored)]");
-                } else {
-                    println!("[END OF STATEMENT]");
-                    program.push_statement();
-                }
-            },
-            "{" => {
-                println!("[PUSH SCOPE]");
-                program.push_instruction(Instruction::Scope(Scope::Push));
-            },
-            "}" => {
-                println!("[POP SCOPE]");
-                program.push_instruction(Instruction::Scope(Scope::Pop));
-            },
-            r"\\" => {
-                println!("[NEWLINE]");
-                program.push_instruction(Instruction::Newline);
+        } else if let Some(txt) = captures.get(RE_TXT_ID) {
+            println!("[Text({})]", txt.as_str());
+            program.push_instruction(Instruction::Txt(txt.as_str().to_owned()))
+        } else if let Some(bra) = captures.get(RE_BRA_ID) {
+            match bra.as_str() {
+                r"\{" => {
+                    println!("[OPEN BRACE]");
+                    program.push_instruction(Instruction::Sym(Symbol::OpenBrace));
+                },
+                r"\}" => {
+                    println!("[CLOSE BRACE]");
+                    program.push_instruction(Instruction::Sym(Symbol::CloseBrace));
+                },
+                _ => unreachable!(),
             }
-            _ => {
-                match Instruction::from_str(word) {
-                    Ok(instruction) => {
-                        println!("[{instruction}]");
-                        program.push_instruction(instruction);
+        }  else if let Some(sco) = captures.get(RE_SCO_ID) {
+            match sco.as_str() {
+                "{" => {
+                    println!("[PUSH SCOPE]");
+                    program.push_instruction(Instruction::Scope(Scope::Push));
+                },
+                "}" => {
+                    println!("[POP SCOPE]");
+                    program.push_instruction(Instruction::Scope(Scope::Pop));
+                },
+                _ => unreachable!(),
+            }
+        } else if captures.get(RE_NLI_ID).is_some() {
+            println!("[NEWLINE]");
+            program.push_instruction(Instruction::Newline);
+        } else if captures.get(RE_END_ID).is_some() {
+            if program.is_current_statement_empty().unwrap_or(true) {
+                println!("[END OF (empty) STATEMENT (ignored)]");
+            } else {
+                println!("[END OF STATEMENT]");
+                program.push_statement();
+            }
+        } else {
+            match {
+                if let Some(cmd) = captures.get(RE_CMD_ID) {
+                    match Macro::from_str(cmd.as_str()) {
+                        Ok(c) => Ok(Instruction::Cmd(c)),
+                        Err(err) => Err(err),
                     }
-                    Err(err) => return Err(err),
+                } else if let Some(var) = captures.get(RE_VAR_ID) {
+                    Ok(Instruction::Var(var.as_str().to_owned()))
+                } else if let Some(sym) = captures.get(RE_SYM_ID) {
+                    match Symbol::from_str(sym.as_str()) {
+                        Ok(s) => Ok(Instruction::Sym(s)),
+                        Err(()) => Err(format!("\"{}\" looks like a symbol, but I don't recognize it", sym.as_str())),
+                    }
+                } else if let Some(num) = captures.get(RE_NUM_ID) {
+                    Ok(Instruction::Num(i32::from_str(num.as_str()).unwrap()))
+                } else {
+                    Err(format!("I don't know what \"{word}\" means"))
                 }
-            },
+            } {
+                Ok(instruction) => {
+                    println!("[{instruction}]");
+                    program.push_instruction(instruction);
+                }
+                Err(err) => return Err(err),
+            }
         }
     }
     Ok(program)
--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,5 +1,9 @@
 use crate::compiler::Program;
 
-pub fn interpret(_code: &Program) {
-
+pub fn interpret(program: &Program) {
+    for statement in &program.statements {
+        for instruction in &statement.instructions {
+            println!("{}", instruction.to_string());
+        }
+    }
 }
